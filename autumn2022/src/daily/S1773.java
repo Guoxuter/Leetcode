4cd9f37681471c3d2fc4f81c5e9acb383cd89b58
guoxuter <guoxuter@gmail.com>
2022-10-29 21:37:25 +0800

    autumn 2022 10.29 new solution S1773

---
+++ autumn2022/src/daily/S1773.java
@@ -0,0 +1,22 @@
+// 2022.10.29
+package daily;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class S1773 {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int index = new HashMap<String, Integer>() {{
+            put("type", 0);
+            put("color", 1);
+            put("name", 2);
+        }}.get(ruleKey);
+        int res = 0;
+        for (List<String> item : items) {
+            if (item.get(index).equals(ruleValue)) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
