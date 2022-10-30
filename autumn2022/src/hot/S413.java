013fc2801308c75684c00f055cc6fdb1b48a32e0
guoxuter <guoxuter@gmail.com>
2022-10-30 17:18:24 +0800

    autumn 2022 10.30 new solution S413

---
+++ autumn2022/src/hot/S413.java
@@ -0,0 +1,24 @@
+// 2022.10.30
+package hot;
+
+public class S413 {
+    public int numberOfArithmeticSlices(int[] nums) {
+        if(nums.length<3)return 0;
+        int l=0;
+        int r=1;
+        int ans=0;
+        int dist=nums[1]-nums[0];
+        while(r<nums.length){
+            if(dist!=nums[r]-nums[r-1]){
+                if(r-l>2){
+                    ans+=(r-l-2)*(r-l-1)/2;
+                }
+                l=r-1;
+                dist=nums[r]-nums[l];
+            }
+            r++;
+        }
+        ans+=(r-l-2)*(r-l-1)/2;
+        return ans;
+    }
+}
