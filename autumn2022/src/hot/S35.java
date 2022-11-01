6a982e11f9f0d30c04986597f34c384dcc4f4cf5
guoxuter <guoxuter@gmail.com>
2022-11-01 14:39:02 +0800

    autumn 2022 11.1 new solution S35

---
+++ autumn2022/src/hot/S35.java
@@ -0,0 +1,17 @@
+// 2022.11.01
+package hot;
+
+public class S35 {
+    public int searchInsert(int[] nums, int target) {
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]<target){
+                l=mid+1;
+            }
+            else r=mid-1;
+
+        }
+        return l;
+    }
+}
